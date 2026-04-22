971643168008c5f1c4346e5fb2c48e604b482d67
dzhumaniyazovdm <gitlab@xrm.ru>
2026-04-22 23:38:05 +0300

    Trigger Firebase deploy

--- src/data/artworks.ts
+++ src/data/artworks.ts
@@ -3,6 +3,8 @@ import streetImage from '../assets/pictures/street.jpg'
 import winterImage from '../assets/pictures/winter.jpg'
 import lakeImage from '../assets/pictures/lake.jpg'
 import portraitImage from '../assets/pictures/portrait.jpg'
+import forestImage from '../assets/pictures/forest.jpg'
+import street1Image from '../assets/pictures/street1.jpg'
 
 export type Artwork = {
   id: number
@@ -42,4 +44,16 @@ export const artworks: Artwork[] = [
     image: portraitImage,
     description: '',
   },
+  {
+    id: 6,
+    title: 'Лес',
+    image: forestImage,
+    description: '',
+  },
+  {
+    id: 7,
+    title: 'Улица в дождь',
+    image: street1Image,
+    description: '', 
+  }
 ]
